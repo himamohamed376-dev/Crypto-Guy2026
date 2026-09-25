4ff52373acada647609cf25ef6dc509af4875763
himamohamed376-dev <himamohamed376@gmail.com>
2026-09-25 09:21:28 +0200

    Create RootLayout component with metadata

---
+++ app/layout.js
@@ -0,0 +1,4 @@
+export const metadata={title:'Crypto Guys 2026'}
+export default function RootLayout({children}){
+  return <html lang="ar" dir="rtl"><body style={{margin:0, background:'#050505'}}>{children}</body></html>
+  }
